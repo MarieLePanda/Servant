fe4d145f4559eb6d83c1c1b4d1734adc67787df8
MarieLePanda <lucas.girardin@outlook.com>
2014-04-19 14:02:13 +0200

    nombre d'attaque par serviteur
    Réglé

--- Exo 2/ExoA.py
+++ Exo 2/ExoA.py
@@ -13,14 +13,13 @@ import random
 
 def main():
 
-    cardSet = loadCardSet("C:\\Users\\lug13995\\Desktop\\ESGI\\Portable Python 3.2.5.1\\Exo 2\\cardSet")
+    cardSet = loadCardSet("C:\\Users\\MKSJ\\Documents\\GitHub\\Servant\\Exo 2\\cardSet")
     playerOne = initPlayer(cardSet)
     playerTwo = initPlayer(cardSet)
 
     print("Player one")
     for cardPlayer in playerOne["field"]:
         printCard(cardPlayer)
-
     print("Player two")
     for cardPlayer in playerTwo["field"]:
         printCard(cardPlayer)
@@ -30,7 +29,6 @@ def main():
             playTrun(playerOne, playerTwo)
         else :
             playTrun(playerTwo, playerOne)
-
     print("Player one")
     for cardPlayer in playerOne["field"]:
         printCard(cardPlayer)
@@ -84,7 +82,6 @@ def loadCardSet(nameFile):
             else:
                 cardSet.append(word)
         listServant.append(loadCard(cardSet[0], cardSet[2], cardSet[1], cardSet[3]))
-
     file.close
     return listServant
 
@@ -98,17 +95,14 @@ def initPlayer(serviteur):
         numCard = random.randint(0, len(serviteur) - 1)
         if(serviteur[numCard] not in hand) :
             hand.append(serviteur[numCard])
-
+            serviteur.remove(serviteur[numCard])
     player = {"health" : 30, "mana" : 1, "hand" : hand, "field" : []}
-
     return player
 
     pass
 
 def playTrun(player, ennemy):
-    """Player can send servant to the field and attack ennemy servant
-        Regler le soucie du servant qui ne doit attaquer qu'une fois.
-    """
+    """Player can send servant to the field and attack ennemy servant"""
 
     choice = False
 
@@ -125,32 +119,26 @@ def playTrun(player, ennemy):
 #Phase d'attaque
 
         servantCanAttack = list(player["field"])
-
-        for cardInField in servantCanAttack :
-
-
+        while 0 < len(servantCanAttack) :
+            print("Nombre de tour d'attaque : ", len(servantCanAttack))
+            print("Avec quel serviteur voulez vous attaquer ?")
+            nameServantAttack = ""
 #Liste des serviteurs du joueur sur le champ de bataille
 
             print("Liste des servants sur le terrain")
-
             for cardPlayer in player["field"] :
                 printCard(cardPlayer)
-
             print("Liste de vos serviteur sur le terrain qui peuvent attaquer")
-
             for cardPlayer in servantCanAttack :
                 printCard(cardPlayer)
-
             while choice == False :
                 nameServantAttack = input("Choisisez le nom du serviteur qui va attaquer")
                 for card in servantCanAttack :
                     if nameServantAttack == card["name"] :
                         choice = True
-
             choice = False
             servantAttack = ""
             for servant in servantCanAttack :
-
                 if servant["name"] == nameServantAttack:
                     servantAttack = servant
                     servantCanAttack.remove(servant)
@@ -158,19 +146,15 @@ def playTrun(player, ennemy):
 #Liste des serviteurs de l'ennemie sur le champ de bataille
 
             print("Liste des serviteur adverse sur le terrain")
-
             for cardPlayer in ennemy["field"] :
                 printCard(cardPlayer)
-
             print("Quel serviteur ennemy voulez vous attaquer ?")
             nameServantTarget = ""
-
             while choice == False:
                 nameServantTarget = input("Choisisez le nom du serviteur a attaquer")
                 for card in ennemy["field"] :
                     if nameServantTarget == card["name"] :
                         choice = True
-
             choice = False
             servantTarget = ""
             for servant in ennemy["field"]:
@@ -188,14 +172,12 @@ def playTrun(player, ennemy):
             else :
                 print("Il reste ",  servantTarget["health"], " points de vie a ", servantTarget["name"])
 
-
 #Phase d'envoie sur le champ de bataille
-    servantCanAttack = list(player["field"])
-    print("Liste de vos serviteur dans votre main")
 
+
+    print("Liste de vos serviteur dans votre main")
     for cardPlayer in player["hand"] :
         printCard(cardPlayer)
-
     if len(player["hand"]) > 0 :
         print("Quel serviteur voulez vous envoyer sur le terrain ?")
         nameServantGoToField = ""
@@ -204,12 +186,12 @@ def playTrun(player, ennemy):
              for card in player["hand"] :
                 if nameServantGoToField == card["name"] :
                     choice = True
-
         for servant in player["hand"]:
             if servant["name"] == nameServantGoToField:
                 player["field"].append(servant)
                 player["hand"].remove(servant)
     servantCanAttack = list(player["field"])
+
     pass
 
 
